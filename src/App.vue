d3fa0e6e7d12c96e17216fdabbc4fb3d92e21941
hanamaruAmeng <1848563105@qq.com>
2020-08-13 15:35:27 +0800

    测试第一次commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <footer-guide v-show="$route.meta.showFooter"></footer-guide>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FooterGuide
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
